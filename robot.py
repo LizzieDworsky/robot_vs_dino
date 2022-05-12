007bd85b94eaa2fb8dc8e7fbe85b3cfaf97479be
LizzieDworsky <amydworsky15@gmail.com>
2022-05-12 14:40:06 -0600

    robot class, changed weapon to a list and added 5 additional weapons with random selection during attack

--- robot.py
+++ robot.py
@@ -1,3 +1,4 @@
+import random
 from weapon import Weapon
 
 
@@ -5,9 +6,10 @@ class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 250
-        self.weapon = Weapon("Arm Cannon", 50)
+        self.weapon = [Weapon("Laser Vision", 50), Weapon("Arm Cannon", 50), Weapon("Energy Claw", 25), Weapon("Energy Cannon", 100), Weapon("Tri Scorpion Cannons", 25), Weapon("Spark Spinner Gun", 25)]
     
     def robo_attack(self, dinosaur):
-        print (f"{self.name} attacks {dinosaur.name} and does {str(self.weapon.attack_power)} damage with his {self.weapon.name}.")
-        dinosaur.health -= self.weapon.attack_power
+        active_weapon = random.choice (self.weapon)
+        print (f"{self.name} attacks {dinosaur.name} and does {str(active_weapon.attack_power)} damage with his {active_weapon.name}.")
+        dinosaur.health -= active_weapon.attack_power
         print (f"{dinosaur.name} now has {str(dinosaur.health)} health left.")
