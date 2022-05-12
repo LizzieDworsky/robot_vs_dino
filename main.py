7742c584c396a846e0345a92326d8fa38f7f9032
LizzieDworsky <amydworsky15@gmail.com>
2022-05-12 11:48:40 -0600

    created files, added user stories

---
+++ main.py
@@ -0,0 +1,6 @@
+#(5 points): As a developer, I want to make at least 7 commits with good, descriptive messages.
+#(5 points): As a developer, I want to make a class for each of the following: 
+# Robot, Dinosaur, Weapon, Battlefield.
+
+#(10 points): As a developer, I want the battle to conclude once either the robot 
+# or the dinosaur has its health points reduced to zero.
