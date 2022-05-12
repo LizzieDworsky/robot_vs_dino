fff03c01b3b44504c2d5c242dac940054ea356ee
LizzieDworsky <amydworsky15@gmail.com>
2022-05-12 12:35:31 -0600

    robot class tested and running

--- dinosaur.py
+++ dinosaur.py
@@ -4,10 +4,10 @@
 
 
 class Dinosaur:
-    def __init__(self):
-        self.name = ""
-        self.attack_power = 0
-        self.health = 0
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = attack_power
+        self.health = 100
     
     def dino_attack (self, robot):
         pass
--- robot.py
+++ robot.py
@@ -5,11 +5,12 @@ from weapon import Weapon
 
 
 class Robot:
-    def __init__(self):
-        self.name = ""
-        self.health = 0
-        self.weapon = Weapon()
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.weapon = Weapon("sword", 10)
     
     def robo_attack(self, dinosaur):
-        pass
-        #void
+        print (f"{self.name} attacks {dinosaur.name} and does {str(self.weapon.attack_power)} damage.")
+        dinosaur.health -= self.weapon.attack_power
+        print (f"{dinosaur.name} now has {str(dinosaur.health)} left.")
