6c463451df8eb9632bcb30d2227648a823e02151
LizzieDworsky <amydworsky15@gmail.com>
2022-05-12 12:11:45 -0600

    weapon class updated

--- weapon.py
+++ weapon.py
@@ -1,7 +1,5 @@
-#(10 points): As a developer, I want a Weapon to have a name and attack_power.
-
 
 class Weapon:
-    def __init__(self):
-        self.name = ""
-        self.attack_power = 0
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = attack_power
